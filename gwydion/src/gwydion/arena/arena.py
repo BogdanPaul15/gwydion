40d784c7b134dbf892057d1c0840f2939614aae9
Prisacaru Bogdan-Paul <paulbp68@gmail.com>
2026-04-10 15:10:37 +0300

    feat: add skeleton for the arena framework

---
+++ gwydion/src/gwydion/arena/arena.py
@@ -0,0 +1,17 @@
+class Arena:
+    """Central framework for tuning, training, testing and comparing RL agents.
+    
+    Attributes:
+        TODO
+    """
+    def __init__(self):
+        """TODO"""
+
+    def tune(self):
+        """TODO"""
+
+    def train(self):
+        """TODO"""
+
+    def test(self):
+        """TODO"""
