6c871eadd31e2fe83f27c84f62587aa3e3d9d57f
Prisacaru Bogdan-Paul <paulbp68@gmail.com>
2026-05-16 22:51:03 +0300

    feat: add a new vectorized action space to be able to make a scaling decision on all deployments at the same time

--- gwydion/src/gwydion/actions/__init__.py
+++ gwydion/src/gwydion/actions/__init__.py
@@ -1,3 +1,4 @@
 from .actions import Action, DoNothing, ScaleDown, ScaleUp
 from .action_registry import build_action_set
-from .action_space import MultiDiscreteAdapter, DiscreteAdapter, build_action_space
+from .action_space import (MultiDiscreteAdapter, DiscreteAdapter,
+                            VectorAdapter, build_action_space)
